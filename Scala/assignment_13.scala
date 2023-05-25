d0df03615934367abef078431757cd83fa45ec1c
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2023-05-25 21:44:28 +0530

    hadoop

---
+++ Scala/assignment_13.scala
@@ -0,0 +1,12 @@
+// Scala program to print Hello World!
+object Geeks {
+  // Main Method
+  def main(args: Array[String]): Unit = {
+    // prints Hello World
+    println("Hello World!")
+
+    // commands to execute scala code
+    // scalac assignment_13.scala
+    // scala Objectname
+  }
+}
